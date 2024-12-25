22bb51b52e96663375f700246cacf9214fd4743e
ashimbhandari5 <ashimbhandari700@gmail.com>
2024-12-25 09:31:30 +0545

    create dto

--- src/futsal/dto/create-futsal.dto.ts
+++ src/futsal/dto/create-futsal.dto.ts
@@ -1,1 +1,35 @@
-export class CreateFutsalDto {}
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateFutsalDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  price: string;
+
+  @IsNotEmpty()
+  @IsString()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  mobile: string;
+
+  @IsNotEmpty()
+  @IsString()
+  location: string;
+
+  @IsNotEmpty()
+  @IsString()
+  photo: string;
+
+  @IsNotEmpty()
+  @IsString()
+  size: string;
+
+  @IsNotEmpty()
+  @IsInt()
+  ownerId: number;
+}
